86cfa1197a9648ab64ee2f488102f785fcbf8172
Nep Net Laptop <catalinbarbuta14@gmail.com>
2022-06-17 11:08:42 +0300

    Removed fake stored procedures

--- src/Data/SQLiteDataAccess.cs
+++ src/Data/SQLiteDataAccess.cs
@@ -7,30 +7,12 @@ namespace RetailManager.Data
 {
 	public class SqLiteDataAccess : IDataAccess
 	{
-		private static Dictionary<string, string> _queries;
-		private static bool _initialized;
-
-		private SqliteConnection Connect()
-		{
-			var connection = new SqliteConnection("Data Source=database.db");
-			if (!_initialized)
-			{
-				var raw = connection.Query<(string Id, string Query)>("SELECT * FROM Queries");
-				_queries = new Dictionary<string, string>();
-				foreach (var (id, query) in raw)
-				{
-					_queries.Add(id, query);
-				}
-				_initialized = true;
-			}
-
-			return connection;
-		}
+		private SqliteConnection Connect() => new SqliteConnection("Data Source=database.db");
 
 		public IEnumerable<Item> GetAllItems()
 		{
 			using var connection = Connect();
-			return connection.Query<Item>(_queries["items.getAll"]);
+			return connection.Query<Item>("SELECT Items.Id, Items.Name, Items.UnitPrice, Items.VatGroup, VatGroups.Vat FROM Items INNER JOIN VatGroups ON Items.VatGroup=VatGroups.VatGroup;");
 		}
 	}
 }
